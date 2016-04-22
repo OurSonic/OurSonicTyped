2ce8fc89216c3b753d3d1f249f46e66f5f2dc4bf
Salvatore <dested@gmail.com>
2016-04-22 13:58:35 -0700

    initial commit

---
+++ Client/app/main.ts
@@ -0,0 +1,14 @@
+﻿/// <reference path="/typings/Compress.d.ts" />
+/// <reference path="/typings/jQuery.d.ts" />
+
+
+class Main {
+    static run() {
+        alert(new Compressor().CompressText("asd"))
+
+    }
+}
+
+console.log(';asd');
+
+Main.run();
